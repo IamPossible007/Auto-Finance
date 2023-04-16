a400e52aab534f370d6e1dc2a979abfe2314f802
SSK-14 <sanjaykumar1481999@gmail.com>
2023-04-16 10:44:44 +0530

    Sanjay: Add about page

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,14 @@
+import { Container } from "@mantine/core";
+import { FeaturesIcons } from "../components/features/FeaturesIcons";
+
+function About() {
+  return (
+    <div>
+      <Container my='md'>
+        <FeaturesIcons />
+      </Container>
+    </div>
+  );
+}
+
+export default About;
--- src/router/RootRouter.jsx
+++ src/router/RootRouter.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home";
 import Loans from "../pages/Loans";
 import { FooterSocial } from "../components/footer/FooterSocial";
 import NotFound from "../pages/NotFound";
+import About from "../pages/AboutUs";
 
 function RootRouter() {
   return (
@@ -12,6 +13,7 @@ function RootRouter() {
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/loans' element={<Loans />} />
+        <Route exact path='/about' element={<About />} />
         <Route exact path='*' element={<NotFound />} />
       </Routes>
       <FooterSocial />
